377ec5b4236777de21eff27847148f49448ae13e
jaypark2 <yuuixing2@gmail.com>
2020-09-06 15:09:07 +0900

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 星裕一郎 on 2020/09/06.
+//  Copyright © 2020 yuichiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
